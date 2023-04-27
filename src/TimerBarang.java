06a3fadaa51c5a813de626fe91030ddc4c8116ad
DzikriMuhammad15 <110533904+DzikriMuhammad15@users.noreply.github.com>
2023-04-28 06:20:37 +0700

    Add files via upload

--- src/TimerBarang.java
+++ src/TimerBarang.java
@@ -4,6 +4,7 @@ public class TimerBarang extends Thread {
     private int sisaWaktu;
     private NonMakanan nonMakanan; // ini adalah object yang dibelinya
     private Sim sim; // sim merupakan sim yang melakukan pembeliannya
+    private Object lock = World.getInstance().getLock();
 
     // kosntruktor
     public TimerBarang(NonMakanan nonMakanan, Sim sim) {
@@ -14,10 +15,23 @@ public class TimerBarang extends Thread {
 
     // run
     public void run() {
-        int waktuSelesai = World.getHari() * 12 + World.getWaktu() + sisaWaktu;
+        World instance = World.getInstance();
+        int waktuSelesai = instance.getHari() * 720 + instance.getWaktu() + sisaWaktu;
         boolean muter = true;
+        // masukin dulu ke dalem on delivery
+        Barang b = nonMakanan;
+        sim.getOnDelivery().add(b);
+
         while (muter) {
-            if (World.getHari() * 12 + World.getWaktu() >= waktuSelesai) {
+            synchronized (lock) {
+                try {
+                    lock.wait();
+                } catch (InterruptedException e) {
+                    System.err.println(e);
+                }
+            }
+            // mengecek jam
+            if (instance.getHari() * 12 + instance.getWaktu() >= waktuSelesai) {
                 muter = false;
             }
         }
@@ -25,11 +39,18 @@ public class TimerBarang extends Thread {
 
         // hapus dari onDelivery
 
-        // ...
+        sim.getOnDelivery().remove(b);
         // ...
 
         // masukkin ke inventory
-        // ...
+        int jumlahSebelumnya;
+        String namaBarang = b.getNama();
+        if (sim.getInventory().containsKey(namaBarang)) {
+            jumlahSebelumnya = sim.getInventory().get(namaBarang);
+        } else {
+            jumlahSebelumnya = 0;
+        }
+        sim.getInventory().put(namaBarang, jumlahSebelumnya + 1);
         // ...
 
     }
