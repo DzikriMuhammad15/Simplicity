ae2a27495b376a780af0d247164dab4e99fe5559
adtyainash <16521107@mahasiswa.itb.ac.id>
2023-04-30 18:45:43 +0700

    add AksiAktif

---
+++ src/main/java/simplicity/AksiAktif.java
@@ -0,0 +1,27 @@
+public interface AksiAktif {
+    
+    public void kerja(int waktu);
+        
+    public void olahraga(int waktu);
+        
+    public void makan(Makanan makanan);
+        
+    public void makan(BahanMakanan bahanMakanan);
+        
+    public void tidur(int waktu);
+        
+    public void masak(Makanan makanan);
+       
+    public void berkunjung(Rumah rumahSim);
+
+    public void buangAir();
+       
+    public void nontonTV(int waktu);
+       
+    public void ngoding(int waktu, String bahasaProgram);
+
+    public void dengerMusik(int waktu, String genre);
+        
+    public void mainGame(int waktu);
+       
+}
