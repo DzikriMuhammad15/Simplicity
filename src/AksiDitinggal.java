c9c7987048ee9ad3ebd6a4992c7e234b8ee45e47
AlanAzizy <103512419+AlanAzizy@users.noreply.github.com>
2023-04-30 14:42:56 +0700

    Delete AksiDitinggal.java

--- src/AksiDitinggal.java
+++
@@ -1,7 +0,0 @@
-public interface AksiDitinggal{
-    public void bunuhDiri();
-
-    public void upgradeRumah(String ruanganBaru, String posisi);
-
-    public void beliBarang(String namaBarang);
-}
