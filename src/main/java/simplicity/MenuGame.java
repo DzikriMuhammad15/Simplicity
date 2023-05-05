3c9f686f4d6f1c70b1554735647576f7bced1e7c
DzikriMuhammad15 <110533904+DzikriMuhammad15@users.noreply.github.com>
2023-05-05 19:47:25 +0700

    Benerin untuk memasang Barang

--- src/main/java/simplicity/MenuGame.java
+++ src/main/java/simplicity/MenuGame.java
@@ -419,7 +419,7 @@ public class MenuGame {
                 j++;
             }    
         }
-        Barang barang = new NonMakanan(namabarang);
+        NonMakanan barang = new NonMakanan(namabarang);
         
         System.out.println("Masukkan titik peletakan barang!");
         System.out.print("X : ");
@@ -473,7 +473,7 @@ public class MenuGame {
                 listObject();
                 System.out.println("masukkan nomor barang");
                 int noBarang = Integer.parseInt(scan.nextLine());
-                Barang barang = new NonMakanan(currSim.getPosisi().getCurrRuangan().getBarangInRuangan().get(noBarang-1).getNama());
+                Barang barang = (currSim.getPosisi().getCurrRuangan().getBarangInRuangan().get(noBarang-1));
                 System.out.println("Masukkan titik pemindahan barang!");
                 System.out.print("X : ");
                 int x=Integer.parseInt(scan.nextLine());
